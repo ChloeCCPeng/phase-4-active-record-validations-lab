9caef7fc012f9e1c8294d4d290250b7044fa9026
Chloe <chloepeng331@icloud.com>
2022-04-06 13:12:06 -0400

    commit tree ee1ac1ea3b89d01a08abee071666ab37e1f4e575

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,28 @@ class Post < ApplicationRecord
     validates :content, length: { minimum: 250 }
     validates :summary, length: { maximum: 250 }
     validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
-    validates :title, exclusion: {in: ["True Facts"]}
+    # validates :title, exclusion: {in: ["True Facts"]}
 
+    validate :clickbait
+
+    # def clickbait
+    #     unless ["Won't Believe", "Secret", "Top [number]", "Guess"].include?(title)
+    #         errors.add(:title, "error")
+    #     end
+    # end
+    def clickbait
+        if [/Won't Believe | Secret | Guess | Top /].none? {|exp| exp.match(title)}
+            errors.add(:title, "needs to be more clickbait-y")
+        end
+    end
+
+    # validate :includes_words
+
+    #custom validations
+
+    # def includes_words
+    #     if [/Won't Believe | Secret | Guess | Top /].none? {|exp| exp.match(title)}
+    #         errors.add(:title, "needs to be more clickbait-y")
+    #     end
+    # end
 end
