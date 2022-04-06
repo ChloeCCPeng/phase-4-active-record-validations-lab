65e602e67556f624ff7f10c3a407828117209c5c
Chloe <chloepeng331@icloud.com>
2022-04-06 13:25:58 -0400

    completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,16 @@
-class Post < ApplicationRecord
+     class Post < ApplicationRecord
     validates :title, presence: true
     validates :content, length: { minimum: 250 }
     validates :summary, length: { maximum: 250 }
     validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
-    validates :title, exclusion: {in: ["True Facts"]}
+    # validates :title, exclusion: {in: ["True Facts"]}
+
+    validate :clickbait
+
+    def clickbait
+        if [/Won't Believe | Secret | Guess | Top /].none? {|exp| exp.match(title)}
+            errors.add(:title, "needs to be more clickbait-y")
+        end
+    end
 
 end
